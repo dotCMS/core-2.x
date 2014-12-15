eb15036a1f6b613fc74c11f0e677cec889b0bae4
Daniel Silva <daniel.silva@dotcms.com>
2014-12-15 16:32:53 -0430

    #72

--- src/com/liferay/portal/servlet/MainServlet.java
+++ src/com/liferay/portal/servlet/MainServlet.java
@@ -295,6 +295,7 @@ public class MainServlet extends ActionServlet {
 		req.setAttribute("dotcache", "no");
 		// Shared session
 
+		
 		HttpSession ses = req.getSession();
 		
 		if (!GetterUtil.getBoolean(PropsUtil.get(PropsUtil.TCK_URL))) {
@@ -305,13 +306,14 @@ public class MainServlet extends ActionServlet {
 			if (sharedSessionId == null) {
 				sharedSessionId = PwdGenerator.getPassword(PwdGenerator.KEY1 + PwdGenerator.KEY2, 12);
 
-				Cookie sharedSessionIdCookie = new Cookie(CookieKeys.SHARED_SESSION_ID, sharedSessionId);
-				sharedSessionIdCookie.setMaxAge(86400);
-				sharedSessionIdCookie.setPath("/");
-				sharedSessionIdCookie.setSecure(Config.getStringProperty("COOKIES_SECURE_FLAG", "https").equals("always") 
-						|| (Config.getStringProperty("COOKIES_SECURE_FLAG", "https").equals("https") && req.isSecure()));
-
-				res.addCookie(sharedSessionIdCookie);
+				String secure = Config.getStringProperty("COOKIES_SECURE_FLAG", "https").equals("always") 
+						|| (Config.getStringProperty("COOKIES_SECURE_FLAG", "https").equals("https") && req.isSecure())?CookieUtil.SECURE:"";
+				
+				String httpOnly = Config.getBooleanProperty("COOKIES_HTTP_ONLY", false)?CookieUtil.HTTP_ONLY:"";
+					
+				StringBuilder headerStr = new StringBuilder();
+				headerStr.append(CookieKeys.SHARED_SESSION_ID).append("=").append(sharedSessionId).append(";").append(secure).append(";").append(httpOnly).append(";Path=/").append(";Max-Age=86400");
+				res.addHeader("SET-COOKIE", headerStr.toString());
 
 				_log.debug("Shared session id is " + sharedSessionId);
 			}
